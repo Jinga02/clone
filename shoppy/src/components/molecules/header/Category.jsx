24fa1568c70b5af4c08c60799543a1b9cbf69c57
Jinga02 <jjh099@icloud.com>
2024-03-20 13:45:50 +0900

    feat : admin일 경우에만 newProducts 컴포넌트 보여주기

--- shoppy/src/components/molecules/header/Category.jsx
+++ shoppy/src/components/molecules/header/Category.jsx
@@ -8,7 +8,7 @@ import { login, logout, onUserStateChange } from "api/firebase";
 import User from "./User";
 
 export default function Category() {
-  const buttonStyle = "px-5 py-2 bg-red-400 text-white";
+  const buttonStyle = "px-5 py-2 bg-red-400 text-white hover:brightness-110";
   const [user, setUser] = useState();
 
   useEffect(() => {
@@ -18,14 +18,17 @@ export default function Category() {
     });
   }, []);
   return (
-    <div className="h-full flex items-center capitalize font-semibold">
+    <div className="h-full flex items-center capitalize font-semibold shrink-0">
       <Link linkUrl="/product" name="products" styleName="" />
       {user && (
         <>
           <Link linkUrl="/carts" name="carts" styleName="" />
-          <Link linkUrl="/product/new" name="newProduct" styleName="">
-            <BsPencilFill />
-          </Link>
+          {user.isAdmin && (
+            <Link linkUrl="/product/new" name="newProduct" styleName="">
+              <BsPencilFill />
+            </Link>
+          )}
+
           <User user={user} />
         </>
       )}
