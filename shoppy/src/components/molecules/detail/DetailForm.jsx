e1a30b1ea027fae1c96eceac6fce720d7ecef67d
Jinga02 <jjh099@icloud.com>
2024-03-22 13:27:54 +0900

    feat : 장바구니 추가 시 react-query 즉시 업데이트, useMutation

--- shoppy/src/components/molecules/detail/DetailForm.jsx
+++ shoppy/src/components/molecules/detail/DetailForm.jsx
@@ -1,31 +1,39 @@
 /** @format */
 
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addOrUpdateToCart } from "api/firebase";
 import Button from "components/atom/Button";
 import { useAuthContext } from "context/Authcontext";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { v4 as uuid } from "uuid";
+
 export default function DetailForm() {
   const location = useLocation();
+  const queryClient = useQueryClient();
   const { id, image, title, price, options } =
     location.state && location.state.product ? location.state.product : {};
   const { user } = useAuthContext();
   const userId = user && user.uid;
   const [selected, setSelected] = useState(options && options[0]);
   const handelSelecte = (e) => setSelected(e.target.value);
-  const handeClick = () => {
-    const product = {
-      id,
-      image,
-      title,
-      price,
-      option: selected,
-      quantity: 1,
-    };
-    console.log(userId, product);
-    addOrUpdateToCart(userId, product);
-  };
+
+  const addCart = useMutation({
+    mutationFn: ({ userId, product }) => addOrUpdateToCart(userId, product),
+    onSuccess: () => queryClient.invalidateQueries([`carts/${userId}/${id}`]),
+  });
+
+  // const addCart = () => {
+  // const product = {
+  //   id,
+  //   image,
+  //   title,
+  //   price,
+  //   option: selected,
+  //   quantity: 1,
+  // };
+  //   addOrUpdateToCart(userId, product);
+  // };
   return (
     <div className="flex flex-col">
       <div className="flex items-center">
@@ -48,7 +56,20 @@ export default function DetailForm() {
       </div>
       <Button
         styleName="w-full py-2 mx-0 my-2 bg-red-400 text-white rounded-lg hover:brightness-110"
-        onClick={handeClick}
+        onClick={() => {
+          const newProduct = {
+            id,
+            image,
+            title,
+            price,
+            option: selected,
+            quantity: 1,
+          };
+          addCart.mutate({
+            userId,
+            product: newProduct,
+          });
+        }}
       >
         장바구니에 추가
       </Button>
